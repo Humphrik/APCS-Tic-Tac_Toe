bd768dac16ece2cfd6007be783932ef3ac156330
Kenneth Humphries <kennyboyELEETE@gmail.com>
2017-06-12 13:14:26 -0400

    Added methods for physical board

--- abstractboard.cpp
+++ abstractboard.cpp
@@ -24,3 +24,36 @@ void aBoard::updateGrid(int pos, int turn) {
     grid[pos/3][pos%3] = turn;
     checkWin();
 }
+
+bool isEmpty(int i) {
+    return (grid[i/3][i%3] == 0);
+}
+/////////////////////////////////
+pBoard::pBoard() {}
+
+void pBoard::printGrid() {
+    for(int i = 0; i < 9; i++) {
+        if(i == 3 || i == 6)
+            cout << endl << "_____";
+        switch(grid[i/3][i%3]) {
+        case 0:
+            cout << " "
+            break;
+        case 1:
+            cout << "X";
+            break;
+        case 2:
+            cout << "O";
+            break;
+        }
+        cout << "|";
+    }
+}
+
+void pBoard::takeTurn() {
+    cout << "Please input a number 1-8";
+    int input;
+    cin >> input;
+    updateGrid(input, playerTurn);
+    printGrid();
+}
